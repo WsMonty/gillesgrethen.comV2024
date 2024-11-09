7175dfebb1626e884511dfad3793f9a9852606ac
Grethen, Gilles <gilles.grethen@realcore.de>
2024-11-09 10:49:55 +0100

    shipping prices

--- src/globals/constants.ts
+++ src/globals/constants.ts
@@ -1,9 +1,9 @@
 export const NEWS_LIMIT = 3;
 
-export const SHIPPING_COST_CD_DE = 3.99;
-export const SHIPPING_COST_CD_EU = 6.49;
-export const SHIPPING_COST_CD_UK_AND_IR = 8.99;
-export const SHIPPING_COST_CD_WORLD = 11.99;
+export const SHIPPING_COST_CD_DE = 1.6;
+export const SHIPPING_COST_CD_EU = 3.7;
+export const SHIPPING_COST_CD_UK_AND_IR = 3.7;
+export const SHIPPING_COST_CD_WORLD = 3.7;
 
 export const SHIPPING_COST_VINYL_DE = 4.79;
 export const SHIPPING_COST_VINYL_EU = 10.49;
