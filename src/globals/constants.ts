281b06c4f145579f791a574aaf54d2df2e064496
Grethen, Gilles <gilles.grethen@realcore.de>
2025-01-27 15:01:52 +0100

    update shipping cost

--- src/globals/constants.ts
+++ src/globals/constants.ts
@@ -1,6 +1,6 @@
 export const NEWS_LIMIT = 3;
 
-export const SHIPPING_COST_CD_DE = 1.6;
+export const SHIPPING_COST_CD_DE = 1.8;
 export const SHIPPING_COST_CD_EU = 3.7;
 export const SHIPPING_COST_CD_UK_AND_IR = 3.7;
 export const SHIPPING_COST_CD_WORLD = 3.7;
