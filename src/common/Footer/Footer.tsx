d63b0cf4d25e5725d8985fd9aae15be6d32fbe67
Grethen, Gilles <gilles.grethen@realcore.de>
2025-12-16 19:31:24 +0100

    - add tidal to footer - remove tooltips on mobile

--- src/common/Footer/Footer.tsx
+++ src/common/Footer/Footer.tsx
@@ -1,66 +1,81 @@
-import './Footer.scss';
-import { FaFacebookF, FaInstagram, FaYoutube, FaSpotify } from 'react-icons/fa';
-import { SiApplemusic } from 'react-icons/si';
-import { Tooltip } from 'react-tooltip';
+import "./Footer.scss";
+import { FaFacebookF, FaInstagram, FaYoutube, FaSpotify } from "react-icons/fa";
+import { SiApplemusic, SiTidal } from "react-icons/si";
+import { Tooltip } from "react-tooltip";
+import { MOBILE_BREAKPOINT } from "../../globals/constants";
+import { useMediaQuery } from "react-responsive";
 
 function Footer() {
+  const isMobile = useMediaQuery({ maxWidth: MOBILE_BREAKPOINT });
+
   return (
     <div className="footer">
-      <Tooltip id="footer-tooltip" />
-      <FaFacebookF
-        className="footer-logo"
-        size={23}
-        onClick={() =>
-          window.open('https://www.facebook.com/gillesgrethen', '_blank')
-        }
-        data-tooltip-id="footer-tooltip"
-        data-tooltip-content="Facebook"
-      />
-      <FaInstagram
-        className="footer-logo"
-        size={25}
-        onClick={() =>
-          window.open('https://www.instagram.com/gilles_grethen_', '_blank')
-        }
-        data-tooltip-id="footer-tooltip"
-        data-tooltip-content="Instagram"
-      />
-      <FaYoutube
-        className="footer-logo"
-        size={30}
-        onClick={() =>
-          window.open(
-            'https://www.youtube.com/channel/UCgZez7ebsQQFIO83RDIj8zQ',
-            '_blank'
-          )
-        }
-        data-tooltip-id="footer-tooltip"
-        data-tooltip-content="Youtube"
-      />
-      <FaSpotify
-        className="footer-logo"
-        size={25}
-        onClick={() =>
-          window.open(
-            'https://open.spotify.com/album/35HwLkp3QIrMYm8BCPYtA5',
-            '_blank'
-          )
-        }
-        data-tooltip-id="footer-tooltip"
-        data-tooltip-content="Spotify"
-      />
-      <SiApplemusic
-        className="footer-logo"
-        size={25}
-        onClick={() =>
-          window.open(
-            'https://geo.music.apple.com/lu/album/_/1727046789?mt=1&app=music&ls=1&at=1000lHKX&ct=odesli_http&itscg=30200&itsct=odsl_m',
-            '_blank'
-          )
-        }
-        data-tooltip-id="footer-tooltip"
-        data-tooltip-content="Apple Music"
-      />
+      {!isMobile && <Tooltip id="footer-tooltip" />}
+      <div className="footer-social-icons">
+        <FaFacebookF
+          className="footer-logo"
+          size={18}
+          onClick={() =>
+            window.open("https://www.facebook.com/gillesgrethen", "_blank")
+          }
+          data-tooltip-id="footer-tooltip"
+          data-tooltip-content="Facebook"
+        />
+        <FaInstagram
+          className="footer-logo"
+          size={21}
+          onClick={() =>
+            window.open("https://www.instagram.com/gilles_grethen_", "_blank")
+          }
+          data-tooltip-id="footer-tooltip"
+          data-tooltip-content="Instagram"
+        />
+        <FaYoutube
+          className="footer-logo"
+          size={25}
+          onClick={() =>
+            window.open(
+              "https://www.youtube.com/channel/UCgZez7ebsQQFIO83RDIj8zQ",
+              "_blank"
+            )
+          }
+          data-tooltip-id="footer-tooltip"
+          data-tooltip-content="Youtube"
+        />
+        <FaSpotify
+          className="footer-logo"
+          size={21}
+          onClick={() =>
+            window.open(
+              "https://open.spotify.com/intl-de/album/73Ak6YJz0GYBuRxZFGODsB",
+              "_blank"
+            )
+          }
+          data-tooltip-id="footer-tooltip"
+          data-tooltip-content="Spotify"
+        />
+        <SiApplemusic
+          className="footer-logo"
+          size={21}
+          onClick={() =>
+            window.open(
+              "https://music.apple.com/de/album/nostalgie/1843596856",
+              "_blank"
+            )
+          }
+          data-tooltip-id="footer-tooltip"
+          data-tooltip-content="Apple Music"
+        />
+        <SiTidal
+          className="footer-logo"
+          size={21}
+          onClick={() =>
+            window.open("https://tidal.com/album/461048750/u", "_blank")
+          }
+          data-tooltip-id="footer-tooltip"
+          data-tooltip-content="Tidal"
+        />
+      </div>
 
       <p className="footer-copyright">
         © Gilles Grethen {new Date().getFullYear()}
